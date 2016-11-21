b8deaa8a3709ca7e0b285a26bc3494f10843e5b8
salavthapa <salav.thapa4@gmail.com>
2016-11-21 12:39:25 +0545

    Add files via upload

---
+++ area of trapzium.c
@@ -0,0 +1,41 @@
+/*
+  Student:salav Thapa
+  Subject:Programing fundamental
+  Roll:
+  Program:area of trapezium
+  Lab Sheet No:4
+  Date:2073-08-06
+  */
+
+  #include <stdio.h>
+        int main(){
+        int a,b,h;
+        float area=0;
+        printf("enter the value of a:\n");
+        scanf("%d",&a);
+        printf("enter the value of b:\n");
+        scanf("%d",&b);
+        printf("enter the value of h:\n");
+        scanf("%d",&h);
+
+
+
+        area=0.5*(a+b)*h;
+        printf("area of trapezium is %f", area);
+        return(0);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
